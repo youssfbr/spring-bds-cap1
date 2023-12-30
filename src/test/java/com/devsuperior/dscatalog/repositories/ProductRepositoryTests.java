3081cb88381373cbd0f8cc933cf0e9656852cc87
Alisson Youssf <youssfbr@gmail.com>
2023-12-30 20:54:32 -0300

    ProductRepositoryTests DeleteWhenIdExists

---
+++ src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscatalog.repositories;
+
+import com.devsuperior.dscatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+
+        long existingId = 1L;
+
+        repository.deleteById(existingId);
+
+        final Optional<Product> result = repository.findById(existingId);
+
+        Assertions.assertFalse(result.isPresent());
+        Assertions.assertTrue(result.isEmpty());
+
+    }
+}
