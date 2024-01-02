74e861b4cff47a1a6d43ee4002f4ba0b850760f8
Alisson Youssf <youssfbr@gmail.com>
2024-01-02 11:50:02 -0300

    ProductResourceIntegrationTests findAllPaged

---
+++ src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIT.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dscatalog.resources;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductResourceIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+    @BeforeEach
+    void setup() throws Exception {
+
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+
+        final ResultActions resultActions = mockMvc
+                .perform(get("/products?page=0&size=12&sort=name,asc", existingId)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        resultActions.andExpect(status().isOk());
+        resultActions.andExpect(jsonPath("$.totalElements").value(countTotalProducts));
+        resultActions.andExpect(jsonPath("$.content").exists());
+        resultActions.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+        resultActions.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+        resultActions.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+
+    }
+}
